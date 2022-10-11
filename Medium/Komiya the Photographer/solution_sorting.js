6cf90d7a248319667d0b8b9b3195ccfa4846dbd5
kenJPG <92031860+kenJPG@users.noreply.github.com>
2022-10-11 22:14:19 +0800

    Update solution_sorting.js

--- Medium/Komiya the Photographer/solution_sorting.js
+++ Medium/Komiya the Photographer/solution_sorting.js
@@ -1,41 +1,13 @@
-// function solve(n, height) {
-// 	height.sort((a, b) => {
-// 		return a - b;
-// 	})
-
-// 	let ans = true;
-// 	for (let i = 0; i < n / 2; i++) {
-// 		ans &= height[i] < height[i + n / 2];
-// 	}
-// 	console.log(ans ? "yes" : "no")
-// }
-
-// function processData(input) {
-// 	let arr = input.split("\n");
-// 	let n = parseInt(arr[0]);
-// 	let height = arr[1].split(" ").map(ele => parseInt(ele));
-// 	solve(n, height);
-// }
-
-// process.stdin.resume();
-// process.stdin.setEncoding("ascii");
-// _input = "";
-// process.stdin.on("data", function (input) {
-// 	_input += input;
-// });
-
-// process.stdin.on("end", function () {
-//    processData(_input);
-// });
-
 function solve(n, height) {
 	height.sort((a, b) => {
 		return a - b;
 	})
 
-	if (height[parseInt(n / 2) - 1] > height[parseInt(n / 2) - 1]) {
-		console.log()
+	let ans = true;
+	for (let i = 0; i < n / 2; i++) {
+		ans &= height[i] < height[i + n / 2];
 	}
+	console.log(ans ? "yes" : "no")
 }
 
 function processData(input) {
@@ -55,5 +27,3 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
-
-
